985e49858b3f64749eb3d72cb3bdba9088b0f28c
MaryRicucci <maria.ricucci.12@gmail.com>
2025-07-12 16:43:22 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Esercizio 1 – Stampa alternata tra due thread
+
+        Due thread devono stampare alternatamente i numeri da 1 a 20, uno stampa i dispari, l'altro i pari. Usare due semafori per garantire l'alternanza corretta.
+
+    }
+}
